8d50cb4097a23d3caf28db5d13344999f802c0aa
andressa1010 <andressa.ns10@gmail.com>
2024-04-28 21:27:35 -0300

    make

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,29 @@ function Home(){
         <header>
          <img className='logo-imagem' src='./src/assets/design-do-logotipo.png' alt='imagem'></img>
          </header>
+         <div className='container-msg'>
+           <img className='logo2' src='./src/assets/makes2.gif' alt='imagem'></img>
+             <p className='texto'>
+                Está cansada da mesma make TODO dia? A gente te entende. 
+                Por isso, nessa semana criamos uma promoção especial para produtos que vão dar um up no seu visual do dia a dia.
+             Entre rímel ou lápis, o que você prefere?
+              Pare o que você está fazendo e 
+             venha agora mesmo aproveitar nossos descontos especiais para itens arrasadores!
+                </p>
+                <h2> Qual é a maquiagem que mais te faz FELIZ? 
+             Conheça a nossa loja e comprove por si mesma a felicidade em formato de MAQUIAGEM.</h2>
+            </div>
+            <div className='container-msg'>
+           <img className='logo2' src='./src/assets/makes.gif' alt='imagem'></img>
+             <p className='texto'>
+             O segredo da maquiagem é revelar o que há de mais belo em você. Dê mais destaque aos seus traços marcantes e encante todo mundo.
+           O look perfeito só fica completo com uma maquiagem à altura. 
+          Maquie-se e desperte o poder que há em você. </p>
+    
+            </div>
+         </div>
         
-        </div>
+    
     )
 }
 
--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -9,8 +9,24 @@ function Produtos(){
             <Header/>
         </div>
         <header>
-        <img className="logo-makes" src="./src/assets/logo-makeup.png" alt="logo"></img>
+            <img className="logo" src="./src/assets/logo-makeup.png" alt="logo"></img>
         </header>
+        <section>
+            <div className="container-logo">
+                <div className="container-imagem">
+                    <img className="capa" src="./src/assets/Capa-batoes.png" alt="imagem"></img>
+                </div>
+                <div className="container-imagem">
+                    <img className="capa" src="./src/assets/capaPaletas.png" alt="imagem"></img>
+                </div>
+                <div className="container-imagem">
+                    <img className="capa" src="./src/assets/CapaBases.png" alt="imagem"></img>
+                </div>
+                <div className="container-imagem">
+                    <img className="capa" src="./src/assets/Capailuminadores.png" alt="imagem"></img>
+                </div>
+            </div>
+        </section>
         <div className="container">
             <div className="produtos-container">
                 <img className="imagem" src="./src/assets/batomVinho.png" alt="imagem"></img>
@@ -85,6 +101,82 @@ function Produtos(){
                 <button className="botao">comprar</button>
             </div>
         </div>
+        <section>
+        <div className="container">
+            <div className="produtos-container">
+                <img className="imagem" src="./src\assets\paleta-colorida-24.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/paleta-colorida-grande.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/paletaRoxa.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/paletaColorida.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/paletaAvon.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/kit-paletas-make.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/baseIntense.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/baseeudora.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/baseeudora.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/baseIntense.png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/BaseBrunaTavares.jpg" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            <div className="produtos-container">
+                <img className="imagem" src="./src/assets/baseMac (2).png" alt="imagem"></img>
+                <img className="avaliacoes" src="src/assets/estrelinhas-avaliacao.png" alt="imagem"></img>
+                <p className="preco">59,99</p>
+                <button className="botao">comprar</button>
+            </div>
+            </div>
+        </section>
         </>
     )
 }
